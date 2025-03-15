d2ddd28aa8a2d248eb507e5859fe6043df6b379c
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-03-15 22:13:12 +0600

    i have added new functionality

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -4,19 +4,20 @@ const hideById = (id) => document.getElementById(id).classList.add('hidden')
 // remove hide by id 
 const showById = (id) => document.getElementById(id).classList.remove('hidden')
 
-hideById('login-page')
-// hideById('navbar')
+hideById('navbar')
+hideById('all-content')
+hideById('faq')
+hideById('all-dynamic-container')
 
 // get Element by id function 
 const getById = (id) => document.getElementById(id)
 // create Element by tag name function 
 const createByTagName = (tagname) => document.createElement(tagname)
 
-hideById('all-dynamic-container')
 
-// Vocabulary Pronunciation 
+// Vocabulary Pronunciation (Japanese)
 function pronounceWord(word) {
-    const utterance = new SpeechSynthesisUtterance(word);
-    utterance.lang = 'en-EN'; // Japanese
-    window.speechSynthesis.speak(utterance);
-  }
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = 'en-EN'; 
+  window.speechSynthesis.speak(utterance);
+}
